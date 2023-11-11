31b152d13e1c34ee25f5a2fdca347e665049f40e
Ilfir <ilfir803@gmail.com>
2023-11-11 14:12:04 +0500

    add repo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
    return <nav className="light-blue lighten-2">
       <div className="nav-wrapper">
-         <a href="!#" className="brand-logo">React Shops</a>
+         <a href="/" className="brand-logo">React Shops</a>
          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+            <li><a href="https://github.com/ILFIR803/react-shop">Repo</a></li>
          </ul>
       </div>
    </nav>
